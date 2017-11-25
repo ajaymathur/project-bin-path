703ed4de0fef8c2e908294218b61ced2290edba8
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2017-11-26 08:27:43 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+const path = require('path');
+const pkgDir = require('pkg-dir');
+
+module.exports = cwd => pkgDir(cwd).then(project => project ? path.resolve(project, 'node_modules', '.bin') : null);
+
+module.exports.sync = cwd => {
+	const project = pkgDir.sync(cwd);
+	return project ? path.resolve(project, 'node_modules', '.bin') : null;
+};
---
+++ test.js
@@ -0,0 +1,12 @@
+'use strict';
+import * as path from 'path';
+import test from 'ava';
+import projectBin from '.';
+
+test('async returns path to bin for packages', async t => {
+	t.is(await projectBin(path.join(__dirname, '__fixatures__', 'foo')), path.join(__dirname, '__fixatures__', 'foo', 'node_modules', '.bin'));
+});
+
+test('sync returns path to bin for packages', t => {
+	t.is(projectBin.sync(path.join(__dirname, '__fixatures__', 'foo')), path.join(__dirname, '__fixatures__', 'foo', 'node_modules', '.bin'));
+});
