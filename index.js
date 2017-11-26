08dcbba317ec5087dc43500cbe3fdb059f3a567a
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2017-11-26 11:19:30 +1100

    updated styling

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const pkgDir = require('pkg-dir');
 module.exports = cwd => pkgDir(cwd).then(project => project ? path.resolve(project, 'node_modules', '.bin') : null);
 
 module.exports.sync = cwd => {
-	const project = pkgDir.sync(cwd);
-	return project ? path.resolve(project, 'node_modules', '.bin') : null;
+  const project = pkgDir.sync(cwd);
+  return project ? path.resolve(project, 'node_modules', '.bin') : null;
 };
--- test.js
+++ test.js
@@ -4,9 +4,9 @@ import test from 'ava';
 import projectBin from '.';
 
 test('async returns path to bin for packages', async t => {
-	t.is(await projectBin(path.join(__dirname, '__fixatures__', 'foo')), path.join(__dirname, '__fixatures__', 'foo', 'node_modules', '.bin'));
+  t.is(await projectBin(path.join(__dirname, '__fixatures__', 'foo')), path.join(__dirname, '__fixatures__', 'foo', 'node_modules', '.bin'));
 });
 
 test('sync returns path to bin for packages', t => {
-	t.is(projectBin.sync(path.join(__dirname, '__fixatures__', 'foo')), path.join(__dirname, '__fixatures__', 'foo', 'node_modules', '.bin'));
+  t.is(projectBin.sync(path.join(__dirname, '__fixatures__', 'foo')), path.join(__dirname, '__fixatures__', 'foo', 'node_modules', '.bin'));
 });
